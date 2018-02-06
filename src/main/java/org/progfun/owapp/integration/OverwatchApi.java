35bf6132c9583d0b3cc75efacee32b55bb04d7d3
Anderson Keiji Matsuki Ito <anderson.ito@yandeh.com.br>
2018-02-06 10:43:21 -0200

    More code comming in

---
+++ src/main/java/org/progfun/owapp/integration/OverwatchApi.java
@@ -0,0 +1,48 @@
+package org.progfun.owapp.integration;
+
+import org.progfun.owapp.data.Hero;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Collection;
+
+@Service
+public class OverwatchApi {
+    private final String USER_AGENT = "Mozilla/5.0";
+
+    public Collection<Hero> listHeros() {
+        return null;
+    }
+
+    private String sendGet(String url) throws IOException {
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+
+        if(responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            return response.toString();
+        }
+
+        return "";
+    }
+}
